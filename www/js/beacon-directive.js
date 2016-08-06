22fdcd2bbbdb1206083c943e90533679e5ace520
Scott Burton <spurton@gmail.com>
2016-08-06 14:09:59 -0700

    Add the needs to the list on toggle

--- www/js/beacon-directive.js
+++ www/js/beacon-directive.js
@@ -24,24 +24,31 @@ angular.module('beacon', [])
         elem.bind('click', launchModal);
 
         scope.needs = [
-          'Clothing',
-          'Financial',
-          'Food/Water',
-          'Housing',
-          'Laundry',
-          'Medical Expertise',
-          'Physical Labor',
-          'Toiletries',
-          'Transportation',
-          'Counseling',
-          'Lost Home'
+          { text: 'Clothing', checked: false},
+          { text: 'Financial', checked: false},
+          { text: 'Food/Water', checked: false},
+          { text: 'Housing', checked: false},
+          { text: 'Laundry', checked: false},
+          { text: 'Medical Expertise', checked: false},
+          { text: 'Physical Labor', checked: false},
+          { text: 'Toiletries', checked: false},
+          { text: 'Transportation', checked: false},
+          { text: 'Counseling', checked: false},
+          { text: 'Lost Home', checked: false}
         ];
 
         scope.saveBeacon = function () {
           scope.modal.hide();
-          console.log(scope.beacon)
-          scope.save()(scope.beacon);
+          console.log(scope.beacon);
         };
+
+        scope.saveNeed = function(need) {
+          if (need.checked) {
+            scope.beacon.needs.push(need);
+          } else {
+            scope.beacon.needs.pop(need);
+          }
+        }
 
         function launchModal() {
           $ionicModal.fromTemplateUrl('templates/beacon-modal.html', {
